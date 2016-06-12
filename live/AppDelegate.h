e122fdf1143ed7b62817c89f4364a6739d279f38
jw <jw@junglewind.com>
2016-06-12 14:38:13 +0800

    Initial Commit

---
+++ live/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  live
+//
+//  Created by Jungle Wind on 6/12/16.
+//  Copyright © 2016 Jungle Wind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
